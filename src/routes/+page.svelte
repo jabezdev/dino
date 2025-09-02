f0c82e1e1d300ea48dd2ff99cda9747415c66729
Jabez Ayson <aysonjabezm@gmail.com>
2025-09-02 23:57:18 +0800

    Trying to add a user input to add a dino

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,38 @@
 <script lang="ts">
   import type { PageProps } from "./$types";
 
+  // Receive data prop
   let { data }: PageProps = $props();
-  let dinosaurs = data.dinosaurs;
+
+  // Make dinosaurs reactive
+  let dinosaurs = $state(data.dinosaurs);
+
+  // Make newDino reactive
+  let newDino = $state("");
+
+  function addDino() {
+    if (newDino.trim()) {
+      dinosaurs = [...dinosaurs, { name: newDino }];
+      newDino = "";
+    }
+  }
 </script>
 
 <main>
   <h1>🦕 Dinosaur app</h1>
   <p>Click on a dinosaur below to learn more.</p>
+
+  <input
+    type="text"
+    bind:value={newDino}
+    placeholder="Add a dinosaur name..."
+  />
+  <button onclick={addDino}>Add Dinosaur</button>
+
   {#each dinosaurs as dinosaur (dinosaur.name)}
-  <a href="/{dinosaur.name.toLowerCase()}" class="dinosaur">
-    {dinosaur.name}
-    <br>
-  </a>
+    <a href="/{dinosaur.name.toLowerCase()}" class="dinosaur">
+      {dinosaur.name}
+      <br>
+    </a>
   {/each}
 </main>
